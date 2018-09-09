d879df13aeb10dcc5a9d52f81b3c1a596b4a502c
Martín Santibáñez <martin.santibanez.a@gmail.com>
2018-09-09 03:33:17 -0300

    close #1

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // Usando jQuery
 $().ready(function() {
-    // alert("Documento listo!");
-    console.log("Listo!");
+    const APIURL = 'http://localhost:8000';
+    cargarTareas();
+    function cargarTareas() {
+        // Haciendo llamada Ajax al backend
+        console.log("Iniciando llamada Ajax");
+        $.get( APIURL + '/tareas', function(data, textStatus, jqXHR) {
+            data.forEach( element => {
+                console.log(element);
+                appendTarea(element);
+            });         
+            console.log("Ajax exitoso!");
+            // console.log(data);
+        }).fail(function(x) {
+            console.log("Error en la llamada Ajax :(");
+            console.log(x);
+        });
+    }
 
-    // Actualizamos el texto del elemento con id 'frase'
-    $('#frase').text("Hola!");
-
-    // Haciendo llamada Ajax al backend
-    console.log("Iniciando llamada Ajax");
-    $.get('http://localhost:8000/tareas', function(data, textStatus, jqXHR) {
-    /** Caso exitoso
-    El m�todo .get solo permite manejar el caso exitoso.
-    Para manejar errores hay que usar $.ajax
-    Ver documentaci�n... http://api.jquery.com/jquery.ajax/ **/                
-
-        console.log("Ajax exitoso!");
-        console.log(data);
-
-    }).fail(function(x) {
-        console.log("Error en la llamada Ajax :(");
-        console.log(x);
-    });
+    function appendTarea( tarea ) { 
+        var tRow = '';
+        tRow += '<tr>';
+        tRow += '<th scope="row">' + tarea.id + '</th>';
+        tRow += '<td><a href="#' + tarea.id + '">' + tarea.titulo + '</a></td>';
+        tRow += '<td>' + tarea.nombre_estado + '</td>';
+        tRow += '</tr>';
+        $('#tareas tbody').append( $( tRow ) );
+    }
 });
